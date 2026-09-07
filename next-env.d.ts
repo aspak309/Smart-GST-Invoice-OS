939a2322f70852a426bc0346e19ea42555fe58a4
aspak309 <aspak0131@gmail.com>
2026-09-07 16:27:45 +0530

    Delete next-env.d.ts

--- next-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
-// NOTE: This file should not be edited manually.
-// It is used by Next.js to provide TypeScript type definitions.
